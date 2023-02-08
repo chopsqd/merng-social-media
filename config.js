12f9afaf03dc9f458fb0b984064fca2350d58c50
Michael Chop <007micle007@gmail.com>
2023-02-08 21:54:56 +0300

    Setted up database and server

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://admin:12345@cluster0.f6mplrp.mongodb.net/merng?retryWrites=true&w=majority'
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+const {ApolloServer} = require('apollo-server')
+const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const Post = require('./models/Post')
+const {MONGODB} = require('./config')
+
+const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+    type Query {
+        getPosts: [Post]
+    }
+`
+
+const resolvers = {
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find()
+                return posts
+            } catch(error) {
+                throw new Error(error)
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => server.listen({port: 5000}))
+    .then(res => console.log(`Server running at ${res.url}`))
