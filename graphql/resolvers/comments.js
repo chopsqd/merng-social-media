6394197d8e17c4cbf0c8a0295ef78e1bcc56b198
Michael Chop <007micle007@gmail.com>
2023-02-10 22:36:36 +0300

    Added a likePost method to the project

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,6 +1,6 @@
 const Post = require("../../models/Post")
 const checkAuth = require('../../utils/check-auth')
-const {UserInputError} = require('apollo-server')
+const {UserInputError, AuthenticationError} = require('apollo-server')
 
 module.exports = {
     Mutation: {
@@ -24,6 +24,24 @@ module.exports = {
                 await post.save()
                 return post
             } else throw new UserInputError('Post not found')
+        },
+        async deleteComment(_, {postId, commentId}, context) {
+            const { username } = checkAuth(context)
+
+            const post = await Post.findById(postId)
+            if(post) {
+                const commentIndex = post.comments.findIndex(comment => comment.id === commentId)
+
+                if(post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1)
+                    await post.save()
+                    return post
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } else {
+                throw new UserInputError('Post not found')
+            }
         }
     }
 }
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("../../models/Post");
 const checkAuth = require('../../utils/check-auth')
-const {AuthenticationError} = require("apollo-server");
+const {AuthenticationError, UserInputError} = require("apollo-server");
 
 module.exports = {
     Query: {
@@ -54,6 +54,26 @@ module.exports = {
             } catch (error) {
                 throw new Error(error)
             }
+        },
+        async likePost(_, {postId}, context) {
+            const {username} = checkAuth(context)
+
+            const post = await Post.findById(postId)
+            if(post) {
+                if(post.likes.find(like => like.username === username)) {
+                    // Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username)
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                await post.save()
+                return post
+            } else throw new UserInputError('Post not found')
         }
     }
 }
